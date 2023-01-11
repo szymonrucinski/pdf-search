06d7bd8b86c7c8e9f5402909671f4e44751ca15a
Szymon Ruciński <szymon.rucinski@visium.ch>
2023-01-11 18:18:20 +0100

    initial commit

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ import qa_pipeline
 import gpt
 
 
-
 def welcome():
     print("Welcome to your source of infinite knowledge")
+
 
 def do_processing(book_location):
     book = preprocess.preprocess(book_location)
     global pipeline
     pipeline = qa_pipeline.build_pipeline(book)
+    return pipeline
+
 
 def query(query):
     answer = gpt.answer_query_with_context(query, pipeline)
     return answer
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     welcome()
     while True:
-        input_value = int(input('\n\nSelect an option please:\n1 to read a book,\n2 to query\n3 to exit\n'))
+        input_value = int(
+            input(
+                "\n\nSelect an option please:\n1 to read a book,\n2 to query\n3 to exit\n"
+            )
+        )
         if input_value == 1:
             input_query = input("Enter the location of your book: ")
             do_processing(input_query)
@@ -30,4 +37,4 @@ if __name__=="__main__":
         elif input_value == 3:
             exit()
         else:
-            print('Please enter a valid input.')
+            print("Please enter a valid input.")
